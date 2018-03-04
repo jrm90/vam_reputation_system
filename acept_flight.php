4823d8d97cc359ea4908ef0d9557c0c3904e2187
jrm90 <37041605+jrm90@users.noreply.github.com>
2018-03-04 15:52:41 +0100

    Update acept_flight.php

--- acept_flight.php
+++ acept_flight.php
@@ -1,15 +1,4 @@
 <?php
-	/**
-	 * @Project: Virtual Airlines Manager (VAM)
-	 * @Author: Alejandro Garcia
-	 * @Web http://virtualairlinesmanager.net
-	 * Copyright (c) 2013 - 2016 Alejandro Garcia
-	 * VAM is licenced under the following license:
-	 *   Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
-	 *   View license.txt in the root, or visit http://creativecommons.org/licenses/by-nc-sa/4.0/
-	 */
-?>
-<?php
 	include('./classes/class_vam_mailer.php');
   // calcular las finanzas del vuelo
 
